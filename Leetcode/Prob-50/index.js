9b3cc521bafeb576be19f3a92e9837135f59386c
imkeyurdesai <keyurdesai124@gmail.com>
2026-07-21 13:08:28 +0530

    [solved] problem 50

---
+++ Leetcode/Prob-50/index.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    return Math.pow(x,n)
+};
+
+const x = 2.10000, n = 3
+console.log(myPow(x,n))
