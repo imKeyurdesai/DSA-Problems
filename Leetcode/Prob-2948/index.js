f018c94b1a8c48099e91ca3bfebe996e28d68d2c
imkeyurdesai <keyurdesai124@gmail.com>
2026-08-29 12:52:16 +0530

    [skipped] problem 2948

---
+++ Leetcode/Prob-2948/index.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @param {number} limit
+ * @return {number[]}
+ */
+const lexicographicallySmallestArray = (A, limit) => {
+    const sort = A.slice().sort((a, b) => a - b);
+    const B = [];
+    const map = new Map();
+    let id = -1;
+
+    for (let i = 0; i < sort.length; i++) {
+        if (i === 0 || sort[i] - sort[i - 1] > limit) {
+            B.push([]);
+            id++;
+        }
+
+        B[id].push(sort[i]);
+        map.set(sort[i], id);
+    }
+
+    const idx = new Int32Array(B.length);
+
+    for (let i = 0; i < A.length; i++) {
+        const cur = map.get(A[i]);
+        A[i] = B[cur][idx[cur]];
+        idx[cur]++;
+    }
+
+    return A;
+};
+
+const nums = [1, 5, 3, 9, 8]
+const limit = 2
+console.log(lexicographicallySmallestArray(nums, limit))
