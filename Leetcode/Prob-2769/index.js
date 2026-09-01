cbd01749ed6ba551debd88513b4352208195c89d
imkeyurdesai <keyurdesai124@gmail.com>
2026-09-01 12:12:29 +0530

    [solved] problem 2769

---
+++ Leetcode/Prob-2769/index.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} num
+ * @param {number} t
+ * @return {number}
+ */
+var theMaximumAchievableX = function(num, t) {
+    return num + t*2
+};
+
+const num = 4
+const t = 1
+console.log(theMaximumAchievableX(num,t))
