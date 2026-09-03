ca47625761106f628c1ac0d14b2c36d8e985fbb9
imkeyurdesai <keyurdesai124@gmail.com>
2026-09-03 14:02:25 +0530

    [solved] problem 3876

---
+++ Leetcode/Prob-3876/index.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums1
+ * @return {boolean}
+ */
+var uniformArray = function (nums1) {
+    let minOdd = Infinity, minEven = Infinity;
+
+    for (const x of nums1) {
+        if (x % 2 === 0) minEven = Math.min(minEven, x);
+        else minOdd = Math.min(minOdd, x);
+    }
+
+    if (minOdd === Infinity || minEven === Infinity) return true;
+
+    return minOdd < minEven;
+};
+
+const nums1 = [1, 4, 7]
+console.log(uniformArray(nums1))
