2d5eb4fe377bc3e75909e4b4e2b4a1088ecbf49c
imkeyurdesai <keyurdesai124@gmail.com>
2026-09-14 20:27:02 +0530

    [solved] problem 386

---
+++ Leetcode/Prob-836/index.js
@@ -0,0 +1,7 @@
+/**
+ * @param {number[]} rec1
+ * @param {number[]} rec2
+ * @return {boolean}
+ */
+const isRectangleOverlap = ([x1, y1, x2, y2], [X1, Y1, X2, Y2]) =>
+    x1 < X2 && X1 < x2 && y1 < Y2 && Y1 < y2;
